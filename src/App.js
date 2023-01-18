45abc207d51f8bf98048b02a77f8c38a13eaf62e
llwslc <llwslc@gmail.com>
2023-01-18 15:57:19 +0800

    inject web3 & ethers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
+import Web3 from "web3/dist/web3.min.js";
 import './App.css';
 
 function App(props) {
@@ -28,6 +29,15 @@ function App(props) {
 
     setInputValue(e.target.value);
   };
+
+  useEffect(() => {
+    window.ethers = ethers
+    window.Web3 = Web3
+    if (window.ethereum) {
+      window.provider = new ethers.providers.Web3Provider(window.ethereum);
+      window.web3 = new Web3(window.ethereum);
+    }
+  }, [])
 
   return (
     <div className="App">
